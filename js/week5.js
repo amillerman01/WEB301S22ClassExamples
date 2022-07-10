25008fe5d72801e1da1cd308a588fa7ed79cddb5
Alex Millerman <90463478+amillerman01@users.noreply.github.com>
2022-07-10 16:56:05 -0400

    Added swipeable and mobile navigation examples

---
+++ js/week5.js
@@ -0,0 +1,15 @@
+$(function () {
+  /* Scrolls the scrollable area manually when you click on the button */
+  $("#scroll-button").on("click", function () {
+    $(".wide-scrolling-wrapper").scrollLeft(300);
+  });
+
+  /* Displays the amount the scrollable area has been scrolled horizontally */
+  $(".wide-scrolling-wrapper").on("scroll", function () {
+    $("#scroll-distance").text($(this).scrollLeft());
+  });
+
+  $("#mobile-hamburger").on("click", function () {
+    $("nav").slideToggle();
+  });
+});
